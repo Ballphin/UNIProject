000787b7df968ad2bfc50356b9b4ff492c165b86
Nanachiser <Nanachiser@users.noreply.github.com>
2024-04-06 03:15:12 -0400

    Fixed Major Forum Issues

--- main.py
+++ main.py
@@ -27,10 +27,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+#Login Manager
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+#Home Page
 @app.route('/')
 def home():
     if current_user.is_authenticated:
